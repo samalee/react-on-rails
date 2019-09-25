495f0976a3c6310b339dff32dfac530f5aeeac23
Samantha Malay <samaleemalay@gmail.com>
2019-09-24 18:32:55 -0600

    rails end done for now

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -1,2 +1,36 @@
 class Api::ItemsController < ApplicationController
+  def index
+    # @items = Item.all
+    render json: Item.all
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      render json: @item
+    else
+      render json: { errors: @items.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    # if @item.update(item_params)
+    #   render json: @item
+    # else
+    #   render json: { errors: @items.errors }, status: :unprocessable_entity
+    # end
+    @item.update(complete: !@item.complete)
+    render json: @item
+  end
+
+  def destroy
+    Item.find(params[:id]).destroy
+    render json: { message: 'Item deleted'}
+  end
+
+  private
+    def item_params
+      params.require(:item).permit(:todo_name, :complete)
+    end
 end
